3ebe92bb1f687191c951af7f8523d6d7d7996dcb
Elias_303 <brightporse@gmail.com>
2023-09-06 20:39:58 -0300

    entidad inventario creada exitosamente

---
+++ src/main/java/imb/pr3/lh/LhApplication.java
@@ -0,0 +1,13 @@
+package imb.pr3.lh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imb/pr3/lh/LhApplicationTests.java
@@ -0,0 +1,13 @@
+package imb.pr3.lh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
