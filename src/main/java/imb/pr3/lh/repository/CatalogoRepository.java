111453c9666e6309b9206ae0e68a0163d922170a
Lore9022 <85725340+Lore9022@users.noreply.github.com>
2023-09-13 21:22:09 -0300

    Primer avance configuracion de Catalogo

---
+++ src/main/java/imb/pr3/lh/repository/CatalogoRepository.java
@@ -0,0 +1,8 @@
+package imb.pr3.lh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import imb.pr3.lh.entity.Catalogo;
+
+public interface CatalogoRepository extends JpaRepository<Catalogo, Integer> {
+
+}
