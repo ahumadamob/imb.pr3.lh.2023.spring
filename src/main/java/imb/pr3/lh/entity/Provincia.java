638def9dea4e8e6582046a114b21035164e87092
AgostinaauDANO <agostinaaudano@gmail.com>
2023-10-10 21:34:36 -0300

    modificado

---
+++ src/main/java/imb/pr3/lh/entity/Provincia.java
@@ -0,0 +1,28 @@
+package imb.pr3.lh.entity;
+
+
+public class Provincia {
+	 private Integer id;
+	 private String nombre;
+	 private Integer provinciaId;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getProvinciaId() {
+		return provinciaId;
+	}
+	public void setProvinciaId(Integer provinciaId) {
+		this.provinciaId = provinciaId;
+	}
+	 
+	 
+}
