f5fa5b7563a8ba34fd7764cf75f621b0bf39f1ab
AgostinaauDANO <agostinaaudano@gmail.com>
2023-10-10 23:06:22 -0300

    service JPA creado

---
+++ src/main/java/imb/pr3/lh/service/jpa/ProvinciaServiceJPA.java
@@ -0,0 +1,40 @@
+package imb.pr3.lh.service.jpa;
+
+import java.util.List;
+
+import imb.pr3.lh.entity.Provincia;
+import imb.pr3.lh.service.ProvinciaService;
+
+public class ProvinciaServiceJPA implements ProvinciaService {
+
+	@Override
+	public List<Provincia> mostrarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Provincia mostrarPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Provincia crearProvincia(Provincia provincia) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Provincia modificarProvincia(Integer id, Provincia provincia) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminarProvincia(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
