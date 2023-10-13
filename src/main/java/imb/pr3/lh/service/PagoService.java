202e386497287ec6a2350334228b691c6e39d797
calabretto <francalabretto@gmail.com>
2023-10-13 18:46:09 -0300

    modificacion PagoService

--- src/main/java/imb/pr3/lh/service/PagoService.java
+++ src/main/java/imb/pr3/lh/service/PagoService.java
@@ -4,9 +4,10 @@ import java.util.List;
 import imb.pr3.lh.entity.Pago;
 
 public interface PagoService {
-    List<Pago> mostrarTodos();
-    Pago mostrarPorId(Integer id);
-    Pago crearPago(Pago pago);
-    Pago modificarPago(Integer id, Pago pago);
-    void eliminarPago(Integer id);
+    List<Pago> buscarTodos();
+    public Pago buscarPorId(Integer id);
+    public Pago guardar(Pago pago);
+    public void eliminar(Integer id);
+    public boolean existe(Integer id);
+    
 }
