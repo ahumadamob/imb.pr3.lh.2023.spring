d770de5d3219c17a6af36b1567c0f0d9eceb3060
Evan Anyi <soleevaanyi@gmail.com>
2023-10-18 00:54:34 -0300

    Se implementan los cambios en Pais service

--- src/main/java/imb/pr3/lh/service/IPaisService.java
+++ src/main/java/imb/pr3/lh/service/IPaisService.java
@@ -5,9 +5,19 @@ import java.util.List;
 import imb.pr3.lh.entity.Pais;
 
 public interface IPaisService {
-	List<Pais> buscarTodos();
-	Pais buscarPorId(Integer id);
-	void guardar(Pais pais);
-	void eliminar(Integer id);
-
+	
+	/*  Obtiene y devuelve una lista de todos los países.*/
+	public List<Pais> buscarTodos();
+	
+	/*  Busca y devuelve un país por su ID.*/
+	public Pais buscarPorId(Integer id);
+	
+	/*  Guarda un país en la base de datos o actualiza uno existente.*/
+	public Pais guardar(Pais pais);
+	
+	/*  Elimina un país de la base de datos por su ID.*/
+	public Pais eliminar(Integer id);
+	
+	/* Verifica si un país con el ID especificado existe en la base de datos.*/
+	public boolean existe(Integer id);
 }
