ff2d0c1a2918ae249753c3420304883002d46166
Evan Anyi <soleevaanyi@gmail.com>
2023-10-24 21:16:58 -0300

    actualizacion

--- src/main/java/imb/pr3/lh/service/IPaisService.java
+++ src/main/java/imb/pr3/lh/service/IPaisService.java
@@ -6,18 +6,18 @@ import imb.pr3.lh.entity.Pais;
 
 public interface IPaisService {
 	
-	/*  Obtiene y devuelve una lista de todos los países.*/
+	
 	public List<Pais> buscarTodos();
 	
-	/*  Busca y devuelve un país por su ID.*/
+	
 	public Pais buscarPorId(Integer id);
 	
-	/*  Guarda un país en la base de datos o actualiza uno existente.*/
+	
 	public Pais guardar(Pais pais);
 	
-	/*  Elimina un país de la base de datos por su ID.*/
+	
 	public Pais eliminar(Integer id);
 	
-	/* Verifica si un país con el ID especificado existe en la base de datos.*/
+	
 	public boolean existe(Integer id);
 }
